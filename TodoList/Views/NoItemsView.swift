adf334fb9f8adf450cf1927e22e2535194911762
manishbajra2000 <mantu.bajra2000@gmail.com>
2024-09-17 16:26:20 +0545

    Added User Experience and Animations

---
+++ TodoList/Views/NoItemsView.swift
@@ -0,0 +1,72 @@
+//
+//  NoItemsView.swift
+//  TodoList
+//
+//  Created by Manish Harsha Bajracharya on 17/09/2024.
+//
+
+import SwiftUI
+
+struct NoItemsView: View {
+    
+    @State var animate: Bool = false
+    let secondaryAccentColor = Color("SecondaryAccentColor")
+    
+    var body: some View {
+        ScrollView {
+            VStack (spacing: 10) {
+                Text("There are no items!")
+                    .font(.title)
+                    .fontWeight(.semibold)
+                Text("Are you a productive person? Add items to your todo list!")
+                    .padding(.bottom, 20)
+                
+                NavigationLink(
+                    destination: AddView(),
+                    label: {
+                        Text("Add something 🥳")
+                            .foregroundColor(.white)
+                            .font(.headline)
+                            .frame(height: 55)
+                            .frame(maxWidth: .infinity)
+                            .background(animate ? secondaryAccentColor : Color.accentColor)
+                            .cornerRadius(15)
+                    }
+                )
+                .padding(.horizontal, animate ? 30: 50)
+                .shadow(
+                    color: animate ? secondaryAccentColor.opacity(0.7) : Color.accentColor.opacity(0.7),
+                    radius: animate ? 30 : 15
+                )
+                .scaleEffect(animate ? 1.1 : 1.0)
+                .offset(y: animate ? -7 : 0)
+            }
+            .multilineTextAlignment(.center)
+        }
+        .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, maxHeight: .infinity)
+        .padding(40)
+        .onAppear(perform: addAnimation)
+    }
+    
+    func addAnimation() {
+        guard !animate else { return }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            withAnimation(
+                Animation
+                    .easeInOut(duration: 1.0)
+                    .repeatForever()
+            ) {
+                animate.toggle()
+            }
+        }
+    }
+    
+}
+
+#Preview {
+    NavigationView{
+        NoItemsView()
+            .navigationTitle("Title")
+    }
+    
+}
